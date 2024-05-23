45f27a2127ea954e66e190f3f92b2dd9252e67b3
4kyoune <wangyyputra@gmail.com>
2024-05-23 18:09:37 +0700

    Update Head.tsx

--- src/components/shared/Head.tsx
+++ src/components/shared/Head.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import React from "react";
 import NextHead from "next/head";
 import { WEBSITE_URL } from "@/constants";
@@ -13,7 +12,7 @@ interface HeadProps {
 
 const Head: React.FC<HeadProps> = (props) => {
   const {
-    title = "Exoexs",
+    title = "Kaguya",
     description = "Website xem anime hoàn toàn miễn phí, không quảng cáo.",
     image = "https://i.ibb.co/JnDDN9j/localhost-3000-2.png",
   } = props;
@@ -42,8 +41,8 @@ const Head: React.FC<HeadProps> = (props) => {
 
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="application-name" content="Exoexs" />
-      <meta name="apple-mobile-web-app-title" content="Exoexs" />
+      <meta name="application-name" content="Kaguya" />
+      <meta name="apple-mobile-web-app-title" content="Kaguya" />
       <meta name="theme-color" content="#EF4444" />
       <meta name="msapplication-navbutton-color" content="#EF4444" />
       <meta
